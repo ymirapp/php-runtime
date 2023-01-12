0630a8dc12bea959f8f2586d3b0a784fd98d15d9
Carl Alexander <carlalexander@gmail.com>
2023-01-12 12:41:10 -0500

    test: forgot to write a test for `Accept-Encoding` header

--- tests/Unit/Lambda/Handler/AbstractPhpFpmRequestEventHandlerTest.php
+++ tests/Unit/Lambda/Handler/AbstractPhpFpmRequestEventHandlerTest.php
@@ -30,6 +30,44 @@ class AbstractPhpFpmRequestEventHandlerTest extends TestCase
     use LoggerMockTrait;
     use PhpFpmProcessMockTrait;
 
+    public function testHandleCreatesCompressibleFastCgiHttpResponseWithGzipAcceptEncodingHeader()
+    {
+        $event = $this->getHttpRequestEventMock();
+        $logger = $this->getLoggerMock();
+        $process = $this->getPhpFpmProcessMock();
+
+        $event->expects($this->once())
+              ->method('getHeaders')
+              ->willReturn(['accept-encoding' => ['deflate, gzip']]);
+
+        $event->expects($this->exactly(2))
+              ->method('getPath')
+              ->willReturn('tmp');
+
+        $event->expects($this->once())
+              ->method('getPayloadVersion')
+              ->willReturn('1.0');
+
+        $logger->expects($this->once())
+               ->method('debug');
+
+        $process->expects($this->once())
+                ->method('handle')
+                ->with($this->isInstanceOf(FastCgiRequest::class));
+
+        $handler = $this->getMockForAbstractClass(AbstractPhpFpmRequestEventHandler::class, [$logger, $process, '/']);
+
+        $handler->expects($this->once())
+                ->method('getScriptFilePath')
+                ->with($this->identicalTo($event))
+                ->willReturn('/path');
+
+        $response = $handler->handle($event);
+
+        $this->assertInstanceOf(FastCgiHttpResponse::class, $response);
+        $this->assertTrue($response->isCompressible());
+    }
+
     public function testHandleCreatesFastCgiHttpResponse()
     {
         $event = $this->getHttpRequestEventMock();
